46509cf52a0c7bad54ff3f17c57d14bf367eff55
Claus Eggers Sørensen <es@forthehearts.net>
2024-05-29 10:37:55 +0200

    Required change for Neosolarised plugin

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Neosolarised needs to be called after loading
+vim.cmd.colorscheme("neosolarized")

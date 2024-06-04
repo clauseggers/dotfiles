a711775d5ff9a8c8e05f7a17920ff695e95e01a4
Claus Eggers Sørensen <es@forthehearts.net>
2024-06-04 09:11:01 +0200

    Updated the way Neosolarized is loaded

--- .config/nvim/lua/plugins/neosolarized.lua
+++ .config/nvim/lua/plugins/neosolarized.lua
@@ -2,11 +2,17 @@ return {
   -- Neosolarized colour scheme
   {
     "svrana/neosolarized.nvim",
+    lazy = false,
+    priority = 1000,
     dependencies = { "tjdevries/colorbuddy.nvim" },
     enabled = true,
     opts = {
       comment_italics = true,
       background_set = true,
     },
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme neosolarized]])
+    end,
   },
 }
